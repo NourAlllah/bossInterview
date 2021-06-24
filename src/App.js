c6d8364357afbdfec8ef2d64428c47a898570cbd
Mariam Mostafa <mariammostafakhalafali@gmail.com>
2021-06-24 02:51:16 +0200

    Editprofile

--- src/App.js
+++ src/App.js
@@ -1,69 +1,79 @@
-import React  from 'react'; 
+import React from 'react';
 import './App.css';
 import "./Components/fonawesome";
-import { BrowserRouter as Router,  Redirect,  Route ,Switch} from 'react-router-dom';
-
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 
 /* PAGES */
-import Profile from './Components/Profile/Profile';
+import Profile from './Pages/Employees/Profile';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar/Navbar';
 import HowDoesItWork from './Pages/Howdoesitworks';
 import AboutUs from './Pages/Aboutus';
 import ContactUs from './Pages/Contactus';
 import SubcribeYourMail from './Pages/Subscribemail';
-import  FooterContainer  from './Components/Containers/footer'; 
+import FooterContainer from './Components/Containers/footer';
 import Faqs from './Pages/Faqs';
 import NotFound from './Components/NotFound';
 import signUpIn from './Components/signUpIn';
+import EmployeeFinal from './Pages/Employees/EmployeeFinal';
 
 
 
 
-function App (){
-  
-return (
-<>
 
-<Router>
-    
-     <Navbar/>
 
-  
-     
+
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+
+      <Router>
+
+        <Navbar />
+
+
+
         <Switch>
-            <Route  path='/'  exact component={Home} />
-            <Route path='/how'  component={ HowDoesItWork} />
-            <Route path='/about'   component={AboutUs} />
-            <Route path='/contact'  component={ContactUs} />
-            <Route path='/subscribe'   component={SubcribeYourMail} />
-            <Route path='/faqs' component={Faqs}/>
-            <Route path='/404' component={NotFound}/>
-            <Route path='/signupin' component={signUpIn}/>
-            <Route path='/profile' component= {Profile}/>
-            <Redirect to='/404'/>
-         </Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/how' component={HowDoesItWork} />
+          <Route path='/about' component={AboutUs} />
+          <Route path='/contact' component={ContactUs} />
+          <Route path='/subscribe' component={SubcribeYourMail} />
+          <Route path='/faqs' component={Faqs} />
+          <Route path='/404' component={NotFound} />
+          <Route path='/signupin' component={signUpIn} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/editprofile' component={EmployeeFinal} />
+          <Redirect to='/404' />
+        </Switch>
 
-        <br/>
-    <br/>  <br/>
-    <br/>  <br/>
-    <br/>  <br/>
-    <br/>  <br/>
-    <br/>      
-    <FooterContainer/>  
 
-          
-    <br/>
-    <br/>      
-    </Router>
-    
-    
-</>
-);
+        <br />
+        <br />  <br />
+        <br />  <br />
+        <br />  <br />
+        <br />  <br />
+        <br />
+        <FooterContainer />
+
+
+        <br />
+        <br />
+      </Router>
+
+
+    </>
+  );
 }
 export default App;
- 
+
 /*
 import { AccountBox } from './Components/accountBox/index';
 import  styled from 'styled-components';
@@ -85,4 +95,3 @@ import { SignupForm } from './Components/accountBox/signupForm';
 `;
 */
 
-  
---
+++ src/Pages/Employees/EmployeeFinal.js
@@ -0,0 +1,62 @@
+import Employees from "./Employees";
+import { makeStyles, CssBaseline, createMuiTheme, ThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#fd9a34",
+            light: '#3c44b126'
+        },
+        secondary: {
+            main: "#fd9a34",
+            light: '#f8324526'
+        },
+        background: {
+            default: "#f4f5fd"
+        },
+    },
+    overrides: {
+        MuiAppBar: {
+            root: {
+                transform: 'translateZ(0)'
+            }
+        }
+    },
+    props: {
+        MuiIconButton: {
+            disableRipple: true
+        }
+    }
+})
+
+
+const useStyles = makeStyles({
+    appMain: {
+        paddingLeft: '100px',
+        paddingRight: '100px',
+        width: '100%'
+    }
+})
+
+
+
+function EmployeeFinal() {
+    const classes = useStyles();
+
+    return (
+
+        <ThemeProvider theme={theme}>
+            <div className={classes.appMain}>
+
+
+                <Employees />
+            </div>
+
+            <CssBaseline />
+        </ThemeProvider>
+
+    );
+
+}
+
+export default EmployeeFinal;
